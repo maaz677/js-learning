aeb99cc6db10817b85cb5103724b7863e7fde091
maaz677 <66419987+maaz677@users.noreply.github.com>
2024-05-31 13:59:05 +0000

    container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 19123
+let accountEmail = "killer@gmail.com"
+var accountPassword = "12345"
+accountCity = "lahore"
+let accountState;
+
+// accountId = 0979 // not allowed 
+
+console.log(accountId);
+
+/* 
+var is not used due to errors instead use let
+const cannot be changed
+you can also not use variables as in line no 4 but is not recommended  
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
