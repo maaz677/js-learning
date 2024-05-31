0ea79b032d12dce225c54f7a91b745b9b99cd7b5
maaz677 <66419987+maaz677@users.noreply.github.com>
2024-05-31 15:19:59 +0000

    dataTypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 1+1) // we are using nodejs not a browser
+
+let name = "maaz"
+let age = 16
+let isLoggedIn = false
+let state; // undefined
+
+//  number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
